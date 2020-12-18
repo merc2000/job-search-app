23fef612e444909dab794b4c3ba76836f92ab8c8
merc2000 <44548862+merc2000@users.noreply.github.com>
2020-12-18 13:05:16 +0530

    return job component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React,{}from 'react';
+import React,{useState}from 'react';
 import useFetchJobs from './useFetchJobs';
 import {Container} from 'react-bootstrap'; 
 import './App.css';
 
 function App() {
-  const {jobs,loading,error} = useFetchJobs(params,page);
 
+  const[params,setParams] = useState({});
+  const [page,setPage] = useState(1);
+  const {jobs,loading,error} = useFetchJobs(params,page);
+ 
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map((job)={
+        return <Job key={job.id} job={job}/>
+      })}
       
     </Container>
   );
