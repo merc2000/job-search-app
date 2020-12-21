331b5139cc1497bfcbda8b99e6d45fb5b2e12a2c
merc2000 <44548862+merc2000@users.noreply.github.com>
2020-12-21 14:08:19 +0530

    fixed bug

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import SearchForm from './SearchForm.js';
 import {Container} from 'react-bootstrap'; 
 import './App.css';
 
-function handleParamChange(e){
-  const param = e.target.name;
-  const value = e.target.value;
-  setPage(1)
-  setParams(prevParams =>{
-    return {...prevParams,[param]:value}
-  })
-}
+
 
 function App() {
 
   const[params,setParams] = useState({});
   const [page,setPage] = useState(1);
   const {jobs,loading,error,hasNextPage} = useFetchJobs(params,page);
+  
+  function handleParamChange(e){
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1)
+    setParams(prevParams =>{
+      return {...prevParams,[param]:value}
+    })
+  }
  
   return (
     <Container className="my-4">
