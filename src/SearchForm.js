8de06b6fb55b181b314dccb9524d26534bb02c5f
merc2000 <44548862+merc2000@users.noreply.github.com>
2020-12-21 14:34:05 +0530

    added checkbox

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -4,7 +4,7 @@ import {Form,Col} from 'react-bootstrap';
 export default function SearchForm({params,onParamChange}){
 return(
   <Form className="mb-4">
-    <Form.Row>
+    <Form.Row className="align-items-end">
       <Form.Group as={Col}>
         <Form.Label>Description</Form.Label>
         <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
@@ -13,7 +13,12 @@ return(
         <Form.Label>Location</Form.Label>
         <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
       </Form.Group>
+      <Form.Group as={Col} xs='auto' ClassName='ml-2' >
+        <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" label="Only Full Time" type="checkbox" className="mb-2">
+        </Form.Check>
+      </Form.Group>
     </Form.Row>
-  </Form>
+    </Form>
+
 )
 }
